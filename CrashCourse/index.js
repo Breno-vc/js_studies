2f47fa232320f6b73cd806ae26757ab721910d53
Breno Carvalho <brenovulcun174@gmail.com>
2024-02-09 02:07:06 -0300

    linux adicoes

---
+++ CrashCourse/index.js
@@ -0,0 +1,14 @@
+let students = 31;
+
+/*
+ addition = + 
+subtraction = -
+multiplication = *
+division = /
+powering = **
+getting the rest = %
+*/
+let extraS = students % 3;
+
+console.log(students + 1);
+console.log(extraS);
