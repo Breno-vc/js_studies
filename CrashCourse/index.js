97f6bea1ba4c2170d42ddd326ca44a35aada55f3
Breno Carvalho <brenovulcun174@gmail.com>
2024-02-10 15:28:25 -0300

    alteracoes novas

--- CrashCourse/index.js
+++ CrashCourse/index.js
@@ -1,7 +1,21 @@
-let username;
+const dec = document.getElementById("dec");
+const reset = document.getElementById("reset");
+const inc = document.getElementById("increase");
 
-document.getElementById("mySubmit").onclick = function () {
-  username = document.getElementById("myText").value;
-  console.log(username);
-  document.getElementById("myH1").textContent = `Hello ${username}`;
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+inc.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+dec.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
+
+reset.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
 };
