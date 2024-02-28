b4904f6efff9bb5d7ff37a58cf59a05169be6cd5
Breno Carvalho <brenovulcun174@gmail.com>
2024-02-28 00:55:27 -0300

    Error fix: dividing by 0 shouldn't be allowed

--- CrashCourse/Projeto Calculadora/index.js
+++ CrashCourse/Projeto Calculadora/index.js
@@ -11,6 +11,10 @@ display.value = null;
 function calculate(){
 try{
 display.value = eval(display.value);
+if (eval(display.value == Infinity)){
+display.textContent = "Can't divide by 0";
+throw new Error ("Infinity");
+}
 }
 catch(error){
   display.value =  "Error";
